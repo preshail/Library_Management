ba5c220cc28e839ac0851825d8d1e6cc71a3ff23
preshail <preshailtiwari@gmail.com>
2025-05-27 20:48:44 +0530

    Create Book.java

---
+++ src/net/javaguides/lms/Book.java
@@ -0,0 +1,34 @@
+package net.javaguides.lms;
+
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private boolean isBorrowed;
+
+    public Book(int id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isBorrowed = false;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
+    public boolean isBorrowed() { return isBorrowed; }
+    public void setBorrowed(boolean borrowed) { isBorrowed = borrowed; }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+               "id=" + id +
+               ", title='" + title + '\'' +
+               ", author='" + author + '\'' +
+               ", isBorrowed=" + isBorrowed +
+               '}';
+    }
+}
